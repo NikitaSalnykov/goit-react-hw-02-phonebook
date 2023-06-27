26fd2d1ba3dbf3b43b98cadd7ccc199ceb75c4e5
NikitaSalnykov <salnikov.nkt@gmail.com>
2023-06-27 18:00:46 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,8 @@ export class App extends Component {
   state = {
   contacts: [],
   name: '',
-  number: ''
+  number: '',
+  originalContacts: [],
   }
 
   handleChange = ({ target }) => {
@@ -24,13 +25,11 @@ export class App extends Component {
   onSubmit = (e) => {
     e.preventDefault();
 
-
-    // if (!this.state.name.trim() && !this.state.number.trim()) {
-    //   console.log("error");
-    //   return
-    // }
+    if (!this.state.name.trim() && !this.state.number.trim()) {
+      Notiflix.Notify.failure('Empty input!')
+      return
+    }
     
-
     const newContact = {
       name: this.state.name,
       number: this.state.number,
@@ -44,26 +43,43 @@ export class App extends Component {
     }));
     Notiflix.Notify.success('Contact added successfully')
   }
+  
+  handleFilter = ({ target }) => {
+    const { contacts, originalContacts } = this.state;
+    const filterValue = target.value.toLowerCase();
 
+    const filteredContacts = contacts.filter(
+      contact => contact.name.toLowerCase().includes(filterValue)
+    );
+
+    this.setState({ originalContacts: filteredContacts });
+
+  }
+    
+
+  
   onDeleteBtn = (id) => {
     this.setState(prevState => ({
     contacts: [...prevState.contacts.filter(contact => contact.id !== id)]
     }))
   }
 
+
+
   
-  render() { 
-    return (
-      <>
-        <div>
-          <h2>Phonebook</h2>
-          <Form onSubmit={this.onSubmit} handleChange={this.handleChange} name={this.state.name} number={this.state.number} />
+render() {
+  return (
+    <>
+      <div>
+        <h2>Phonebook</h2>
+        <Form onSubmit={this.onSubmit} handleChange={this.handleChange} name={this.state.name} number={this.state.number} />
           
-          <h2>Contacts</h2>
-          <ContactList contacts={this.state.contacts} onDeleteBtn={this.onDeleteBtn}/>
+        <h2>Contacts</h2>
+        <label htmlFor="filter">Find contacts by name</label>
+        <input type="text" name="input" id="input" onChange={this.handleFilter}/>
+        <ContactList contacts={this.state.originalContacts.length > 0 ? this.state.originalContacts : this.state.contacts} onDeleteBtn={this.onDeleteBtn} />
       </div>
-      </>
-     );
-  }
+    </>
+  );
 }
- 
+}
